7d7d0fbec7e618aa09b7a8ced80d7dc03f21560b
hikiray <satalomaksimka12345678910@gmail.com>
2025-12-22 00:52:36 +0700

    добавил html

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func productsHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+	response := fmt.Sprintf("Product %s", id)
+	fmt.Fprint(w, response)
+}
 
 func main() {
-	fmt.Println("Пока ничего нет:(((")
+
+	router := mux.NewRouter()
+
+	router.HandleFunc("/api/health", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Hello!")
+		fmt.Fprintln(w, "This site working.")
+	})
+
+	http.Handle("/", router)
+	fmt.Println("Server is listening")
+	http.ListenAndServe(":8181", nil)
 }
